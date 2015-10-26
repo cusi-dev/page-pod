e81e61858c84cdcbe404831cc3e146bdbf7511de
Josh Graham <graham.josh@gmail.com>
2015-10-25 21:56:42 -0500

    Improve sample

--- sample/src/home.js
+++ sample/src/home.js
@@ -1,11 +1,18 @@
 import { inject } from "aurelia-framework";
-import { Router } from "aurelia-router";
+import { Router, activationStrategy } from "aurelia-router";
 
 @inject(Router)
 export class Home {
   constructor(router) {
     this.router = router;
-    this.currentPageNumber = 5;
+  }
+
+  activate(params) {
+    this.currentPageNumber = Number(params.page) || 1;
     this.totalPages = 15;
+  }
+
+  determineActivationStrategy(){
+    return activationStrategy.replace;
   }
 }
